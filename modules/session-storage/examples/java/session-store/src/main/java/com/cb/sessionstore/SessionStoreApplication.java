4c20188ae4646a3c656a9852a519e8c3ba175ae3
Denis Rosa <deniswsrosa@gmail.com>
2019-04-08 21:31:28 +0200

    adding Java session store

---
+++ modules/session-storage/examples/java/session-store/src/main/java/com/cb/sessionstore/SessionStoreApplication.java
@@ -0,0 +1,17 @@
+package com.cb.sessionstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.couchbase.config.annotation.web.http.EnableCouchbaseHttpSession;
+
+// tag::enableSession[]
+@SpringBootApplication
+@EnableCouchbaseHttpSession(keepStringAsLiteral = true)
+public class SessionStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionStoreApplication.class, args);
+	}
+
+}
+// end::enableSession[]
